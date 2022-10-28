093571d172c6ec3aa7ea70bc6c9c7244b7d4122f
RandomNameGiven2 <my9090202865@gmail.com>
2022-10-28 09:31:58 +0530

    changed to upper case in 3rd clone

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-       <h1>Hi... this is ABOUT Component</h1>
+       <h1>Hi... this is ABOUT ABOUT Component</h1>
     </div>
   )
 }
